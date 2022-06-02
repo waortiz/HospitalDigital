fdd59e5d0b495ba6f445960e5b0a343b753c127d
SALA235-PC06\Estudiante <waortiz@hotmail.com>
2022-06-02 06:52:51 -0500

    Ajustes

--- HospitalDigital/FormularioIngreso.Designer.cs
+++ HospitalDigital/FormularioIngreso.Designer.cs
@@ -40,10 +40,9 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(337, 421);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancelar.Location = new System.Drawing.Point(259, 310);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(217, 58);
+            this.btnCancelar.Size = new System.Drawing.Size(163, 47);
             this.btnCancelar.TabIndex = 1;
             this.btnCancelar.Text = "C&ancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -51,10 +50,9 @@
             // 
             // btnIngresar
             // 
-            this.btnIngresar.Location = new System.Drawing.Point(640, 421);
-            this.btnIngresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIngresar.Location = new System.Drawing.Point(480, 342);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(217, 58);
+            this.btnIngresar.Size = new System.Drawing.Size(163, 47);
             this.btnIngresar.TabIndex = 2;
             this.btnIngresar.Text = "&Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
@@ -66,60 +64,53 @@
             this.gbUsuario.Controls.Add(this.txtUsuario);
             this.gbUsuario.Controls.Add(this.lblContraseña);
             this.gbUsuario.Controls.Add(this.lblUsuario);
-            this.gbUsuario.Location = new System.Drawing.Point(303, 48);
-            this.gbUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbUsuario.Location = new System.Drawing.Point(227, 39);
             this.gbUsuario.Name = "gbUsuario";
-            this.gbUsuario.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gbUsuario.Size = new System.Drawing.Size(520, 298);
+            this.gbUsuario.Size = new System.Drawing.Size(390, 242);
             this.gbUsuario.TabIndex = 0;
             this.gbUsuario.TabStop = false;
             this.gbUsuario.Text = "Ingreso Datos";
             // 
             // txtContraseña
             // 
-            this.txtContraseña.Location = new System.Drawing.Point(189, 123);
-            this.txtContraseña.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtContraseña.Location = new System.Drawing.Point(142, 100);
             this.txtContraseña.Name = "txtContraseña";
-            this.txtContraseña.Size = new System.Drawing.Size(285, 22);
+            this.txtContraseña.Size = new System.Drawing.Size(215, 20);
             this.txtContraseña.TabIndex = 3;
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(189, 75);
-            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUsuario.Location = new System.Drawing.Point(142, 61);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(285, 22);
+            this.txtUsuario.Size = new System.Drawing.Size(215, 20);
             this.txtUsuario.TabIndex = 1;
             // 
             // lblContraseña
             // 
             this.lblContraseña.AutoSize = true;
-            this.lblContraseña.Location = new System.Drawing.Point(67, 127);
-            this.lblContraseña.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblContraseña.Location = new System.Drawing.Point(50, 103);
             this.lblContraseña.Name = "lblContraseña";
-            this.lblContraseña.Size = new System.Drawing.Size(76, 16);
+            this.lblContraseña.Size = new System.Drawing.Size(61, 13);
             this.lblContraseña.TabIndex = 2;
             this.lblContraseña.Text = "&Contraseña";
             // 
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(67, 75);
-            this.lblUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsuario.Location = new System.Drawing.Point(50, 61);
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(54, 16);
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
             this.lblUsuario.TabIndex = 0;
             this.lblUsuario.Text = "&Usuario";
             // 
             // FormularioIngreso
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.gbUsuario);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormularioIngreso";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ingreso";
